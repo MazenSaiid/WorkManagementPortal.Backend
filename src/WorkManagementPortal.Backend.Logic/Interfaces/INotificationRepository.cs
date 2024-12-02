f1255ebda4caee9a651a1eb7d1699964d8564149
Mazen Saiid <Mazen.Saiid@bnsights.com>
2024-12-03 00:30:42 +0200

    add notification task

---
+++ src/WorkManagementPortal.Backend.Logic/Interfaces/INotificationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkManagementPortal.Backend.Logic.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task SendPasswordResetEmailAsync(string email, string resetLink);
+    }
+}
